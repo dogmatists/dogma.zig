ff5fb8d7369d78adc6deca7267dd129a6124befb
Arto Bendiken <arto@bendiken.net>
2020-07-16 13:25:54 +0300

    Defined Angle.

--- dogma.zig
+++ dogma.zig
@@ -1,1 +1,20 @@
 // This is free and unencumbered software released into the public domain.
+
+pub const Angle = extern struct {
+    radians: f64,
+
+    const Self = @This();
+
+    pub fn init(radians: f64) Self {
+        return Self{.radians = radians};
+    }
+  };
+
+test "Angle" { // zig test --main-pkg-path . dogma.zig
+    const meta = @import("std").meta;
+    const testing = @import("std").testing;
+
+    meta.refAllDecls(@This());
+
+    testing.expect(Angle.init(2).radians == 2);
+}
